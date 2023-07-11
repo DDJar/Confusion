1090140429c459d004e0b3812d41520dc57d8bb3
DDJar <syquocvietoppo@gmail.com>
2023-07-11 17:16:22 +0700

    Lab8

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,222 +1,51 @@
-/* eslint-disable react/jsx-pascal-case */
-import React, { Component } from 'react';
-import {
-    Card, CardImg, CardText, CardBody, CardTitle,
-    Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody,
-    Button, Label, Col, Row
-} from 'reactstrap';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { Control, LocalForm, Errors } from 'react-redux-form';
-import { Loading } from './LoadingComponent';
-import { baseUrl } from '../shared/baseUrl';
-import { FadeTransform, Fade, Stagger } from 'react-animation-components';
-const required = (val) => val && val.length;
-const maxLength = (len) => (val) => !(val) || (val.length <= len);
-const minLength = (len) => (val) => (val) && (val.length >= len);
+function Footer(props) {
+    return(
+    <div className="footer">
+        <div className="container">
+            <div className="row justify-content-center">             
+                <div className="col-4 offset-1 col-sm-2">
+                    <h5>Links</h5>
+                    <ul className="list-unstyled">
+                    <li><Link to='/home'>Home</Link></li>
+                        <li><Link to='/aboutus'>About Us</Link></li>
+                        <li><Link to='/menu'>Menu</Link></li>
+                        <li><Link to='/contactus'>Contact Us</Link></li>
 
-class CommentForm extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isModalOpen: false
-        }
-
-        this.toggleModal = this.toggleModal.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    toggleModal() {
-        this.setState({ isModalOpen: !this.state.isModalOpen });
-    }
-
-    handleSubmit(values) {
-        // console.log("Current State is: " + JSON.stringify(values));
-        // alert("Current State is: " + JSON.stringify(values));
-        this.toggleModal();
-        // this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
-        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment)
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="row mt-3">
-                    <div>
-                        <Button outline onClick={this.toggleModal} >
-                            <span className="fa fa-pencil fa-lg" ></span> <span class="text-dark font-weight-bold">Submit Comment</span>
-                        </Button>
+                    </ul>
+                </div>
+                <div className="col-7 col-sm-5">
+                    <h5>Our Address</h5>
+                    <address>
+                      121, Clear Water Bay Road<br />
+                      Clear Water Bay, Kowloon<br />
+                      HONG KONG<br />
+                      <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
+                      <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
+                      <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
+                         confusion@food.net</a>
+                    </address>
+                </div>
+                <div className="col-12 col-sm-4 align-self-center">
+                    <div className="text-center">
+                        <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
+                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></a>
+                        <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
+                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
+                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
+                        <a className="btn btn-social-icon" href="mailto:"><i className="fa fa-envelope-o"></i></a>
                     </div>
-                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                        <ModalHeader toggle={this.toggleModal}>
-                            <h4 >Submit Comment</h4>
-                        </ModalHeader>
-                        <ModalBody>
-                            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
-                                <Row className="form-group">
-                                    <Col md={12}>
-                                        <Label htmlFor="rating">
-                                            <h6 classs="text-secondary">Rating</h6>
-                                        </Label>
-                                        <Control.select model=".rating" name="rating" className="form-select text-dark ">
-                                            <option>1</option>
-                                            <option>2</option>
-                                            <option>3</option>
-                                            <option>4</option>
-                                            <option>5</option>
-                                        </Control.select>
-                                    </Col>
-                                </Row>
-                                <Row className="form-group">
-                                    <Col md={12}>
-                                        <Label htmlFor="author">
-                                            <h6 classs="text-secondary">Your Name</h6>
-                                        </Label>
-                                        <Control.text model=".author" id="author" name="author" placeholder="Your Name" className="form-control text-dark"
-                                            validators={{
-                                                required, minLength: minLength(3), maxLength: maxLength(15)
-                                            }}
-                                        />
-                                        <Errors
-                                            className="text-danger"
-                                            model=".author"
-                                            show="touched"
-                                            messages={{
-                                                required: 'Required\n',
-                                                minLength: 'Must be greater than 2 characters',
-                                                maxLength: 'Must be 15 characters or less'
-                                            }}
-                                        />
-                                    </Col>
-                                </Row>
-                                <Row className="form-group">
-                                    <Col md={12}>
-                                        <Label htmlFor="comment">
-                                            <h6 classs="text-secondary">Comment</h6>
-                                        </Label>
-                                        <Control.textarea model=".comment" id="comment" name="comment" rows="6" className="form-control text-dark" />
-                                    </Col>
-                                </Row>
-                                <Row className="form-group" style={{ paddingTop: 20, width: 100 }}>
-                                    <Col md={8}>
-                                        <Button type="submit" color="primary" >
-                                            <span class="text-light">Submit</span>
-                                        </Button>
-                                    </Col>
-                                </Row>
-                            </LocalForm>
-                        </ModalBody>
-                    </Modal>
                 </div>
             </div>
-        );
-    }
-}
-
-function RenderDish({ dish }) {
-
-    return (
-        <div className="col-md-12 m-2">
-            <FadeTransform  in
-                transformProps={{
-                    exitTransform: 'scale(0.5) translateY(-50%)'
-                }}>
-                <Card>
-                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            </FadeTransform>
-
+            <div className="row justify-content-center">             
+                <div className="col-auto">
+                    <p>© Copyright 2018 Ristorante Con Fusion</p>
+                </div>
+            </div>
         </div>
-    );
-
+    </div>
+    )
 }
 
-
-// function RenderComments({ comments, addComment, dishId }) {
-function RenderComments({ comments, postComment, dishId }) {
-    return (
-        <div className="col-12 col-md-10 m-2" style={{ fontSize: 20 }}>
-            <h1>Comments</h1>
-            <ul className="list-unstyled">
-            <Stagger in>
-                        {comments.map((comment) => {
-                            return (
-                                <Fade in>
-                                <li key={comment.id}>
-                                <p>{comment.comment}</p>
-                                <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                                </li>
-                                </Fade>
-                            );
-                        })}
-                        </Stagger>
-            </ul>
-            {/* <CommentForm dishId={dishId} addComment={addComment} /> */}
-            <CommentForm dishId={dishId} postComment={postComment} />
-        </div>
-
-    );
-}
-/* ex31 */
-const DishDetail = (props) => {
-    if (props.isLoading) {
-        return (
-            <div className="container">
-                <div className="row">
-                    <Loading />
-                </div>
-            </div>
-        );
-    }
-    else if (props.errMess) {
-        return (
-            <div className="container">
-                <div className="row">
-                    <h4>{props.errMess}</h4>
-                </div>
-            </div>
-        );
-    }
-    else
-        if (props.dish != null) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <div className="col-12">
-                            <h3>{props.dish.name}</h3>
-                            <hr />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            <RenderDish dish={props.dish} />
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            <RenderComments comments={props.comments}
-                                postComment={props.postComment}
-                                dishId={props.dish.id}
-                            />
-
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
-
-}
-
-export default DishDetail;
+export default Footer;
